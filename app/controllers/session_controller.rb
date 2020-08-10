031eb41aafa5df843c6ce85b3fcc35e231a7621c
engahmed10 <eng.computer1@yahoo.com>
2020-08-10 15:12:09 -0700

    added account to navbar

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,7 @@
+class SessionController < ApplicationController
+
+def destroy
+    session.destroy
+    redirect_to root_path
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :teches
   devise_for :users
   root 'teches#index'
-  post '/logout' => "session#destroy"
+  get '/users/sign_out' => "session#destroy"
 
   get 'teches/:id/delete' => "teches#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
