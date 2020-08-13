b0e08e032bca239fc76987d6db847aec2ca56b5b
engahmed10 <eng.computer1@yahoo.com>
2020-08-12 23:43:59 -0700

    admin to user

--- db/schema.rb
+++ db/schema.rb
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 202008080210763) do
     t.string "provider"
     t.string "uid"
     t.string "name"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
